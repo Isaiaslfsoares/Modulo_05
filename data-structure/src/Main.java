5e0da0cd38f8015239d668207d8fd98562157e03
Isaias Soares <165928116+Isaiaslfsoares@users.noreply.github.com>
2024-08-12 19:15:18 -0300

    upload lessons

---
+++ data-structure/src/Main.java
@@ -0,0 +1,10 @@
+import lists.CustomList;
+
+public class Main {
+    public static void main(String[] args) {
+            CustomList list = new CustomList();
+
+            list.add(10);
+        System.out.println(list.toString());
+        }
+    }
